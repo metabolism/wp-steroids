d48a6a3f0ef6dc7447f75516d4e46f8e72eebf87
Jerome Barbato <jerome.barbato@gmail.com>
2022-10-02 17:01:09 +0200

    bugfix

--- includes/plugins/relevanssi.php
+++ includes/plugins/relevanssi.php
@@ -7,37 +7,35 @@
  */
 class WPS_Relevanssi
 {
-    public function fixFoundPosts(){
+	public function fixFoundPosts(){
 
-        if( !function_exists('relevanssi_do_query') )
-            return;
+		if( !function_exists('relevanssi_do_query') )
+			return;
 
-        add_filter( 'posts_pre_query', function( $check, $query ) {
+		add_filter( 'posts_pre_query', function( $posts, $query ) {
 
-            if ( $query->query_vars['relevanssi']??true ) {
+			if ( $query->query_vars['relevanssi']??false ) {
 
-                $posts = relevanssi_do_query( $query );
-                $query->relevanssi_found_posts = $query->found_posts;
+				$posts = relevanssi_do_query( $query );
+				$query->relevanssi_found_posts = $query->found_posts;
+			}
 
-                return $posts;
-            }
+			return $posts;
 
-            return $check;
+		}, 10, 2 );
 
-        }, 10, 2 );
+		add_filter( 'found_posts', function( $found_posts, $query ){
 
-        add_filter( 'found_posts', function( $found_posts, $query ){
+			return $query->relevanssi_found_posts??$query->found_posts;
 
-            return $query->relevanssi_found_posts??$query->found_posts;
+		}, 20, 2 );
+	}
 
-        }, 20, 2 );
-    }
-
-    /**
-     * Construct
-     */
-    public function __construct()
-    {
-        add_action('init', [$this, 'fixFoundPosts']);
-    }
+	/**
+	 * Construct
+	 */
+	public function __construct()
+	{
+		add_action('init', [$this, 'fixFoundPosts']);
+	}
 }
