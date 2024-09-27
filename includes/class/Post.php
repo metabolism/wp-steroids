794143b386be84d5640f5b8fcbbeb49582e7bef2
Jerome Barbato <jerome.barbato@gmail.com>
2024-09-27 10:32:27 +0200

    bugfix

--- includes/class/Post.php
+++ includes/class/Post.php
@@ -12,9 +12,30 @@ class WPS_Post extends Post
      */
     public function excerpt(array $options = []): string
     {
+        //prevent infinite loop
         if( $this->post_excerpt || !has_blocks($this->post_content) )
             return parent::excerpt(array_merge($options,['read_more'=>false]));
         else
             return false;
+    }
+
+    /**
+     * Detect excerpt
+     *
+     * @return bool
+     */
+    public function hasExcerpt(){
+
+        return !empty($this->post_excerpt);
+    }
+
+    /**
+     * Mapper for Metabolism/WordpressBundle compatibility
+     *
+     * @return mixed
+     */
+    public function customField(string $field_name){
+
+        return $this->meta($field_name);
     }
 }
