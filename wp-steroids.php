f4ea05cd5b5e7fd21456c0d126304dd58ef0be1c
Jerome <jerome@akhela.fr>
2025-09-09 15:38:19 +0200

    bugfix css

--- wp-steroids.php
+++ wp-steroids.php
@@ -108,7 +108,7 @@ class WPS{
 
         define('WPS_PATH', __DIR__);
         define('WPS_PLUGIN_URL', plugin_dir_url(__FILE__));
-        define('WPS_VERSION', '1.5.11');
+        define('WPS_VERSION', '1.5.11.1');
 
         require __DIR__ . '/includes/vendor/autoload.php';
 
